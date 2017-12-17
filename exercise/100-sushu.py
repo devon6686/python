30f5551e1745f0e4c23d720b73b77d4acd33cff4
devon6686 <devon6686@126.com>
2017-12-17 15:44:07 +0800

    add 求素数

--- exercise/100-sushu.py
+++ exercise/100-sushu.py
@@ -2,3 +2,18 @@
 """
 求100内的素数
 """
+
+import math
+
+primenumber = []
+flag = False
+for x in range(2, 10000):
+	for i in primenumber:
+		if x % i == 0:
+			flag = True
+			break
+		if i >= math.ceil(math.sqrt(x)):
+			flag = False
+	if not flag:
+		print(x)
+		primenumber.append(x)
